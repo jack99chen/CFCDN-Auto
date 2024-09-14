f193f1609a149836acee23119a3d4496cbc37ecc
eliangwww <96296131+eliangwww@users.noreply.github.com>
2024-09-14 18:49:48 +0800

    Update bestdomain.py

--- bestdomain.py
+++ bestdomain.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
     
     # 示例URL和子域名对应的IP列表
     subdomain_ip_mapping = {
-        'bestproxy': 'https://ipdb.030101.xyz/api/bestproxy.txt',
+        'bestproxy': 'https://ipdb.030101.xyz/api/bestproxybeta.txt',
         # 添加更多子域名和对应的IP列表URL
     }
     
